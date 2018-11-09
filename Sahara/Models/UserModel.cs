e397e15f2b2caa95f75791bfa85418d763cdac98
enrique95ae <enrique95ae@gmail.com>
2018-11-09 13:16:11 -0800

    UserModel

---
+++ Sahara/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Sahara
+{
+    public class User
+    {
+        public int      UserId { get; set; }
+        public string   UserName { get; set; }
+        public string   UserLastName { get; set; }
+        public string   UserPhoneNumber { get; set; }
+        public string   UserEmail { get; set; }
+        public string   Password { get; set; }
+        public string   CreditCardNumber { get; set; }
+        public string   AddressLine1 { get; set; }
+        public string   AddressLine2 { get; set; }
+        public int      ZipCode { get; set; }
+        public string   State { get; set; }
+    }
+}
