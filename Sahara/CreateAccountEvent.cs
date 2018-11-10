a6f21888ba9bdcd6d39e057027b8965fb08999f8
enrique95ae <enrique95ae@gmail.com>
2018-11-09 18:12:55 -0800

    renamed emptyfile

---
+++ Sahara/CreateAccountEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ProtoBuf;
+
+namespace CreateAccountEvent{
+
+    public class CreateAccountEvent : BaseEvent
+    {
+        [ProtoMember(1)]
+        public string UserEmail { get; set; }
+
+        [ProtoMember(2)]
+        public string UserPassword { get; set; }
+    }
+
+    public CreateAccountEvent()
+    {
+        Type = EventType.CreateAccount;
+        UserEmail = null;
+        UserPasword = null;
+    }
+
+    public CreateAccountEvent(string email, string password)
+    {
+        Type = EventType.CreateAccount;
+        UserEmail = email;
+        UserPasword = password;
+    }
+
+}
