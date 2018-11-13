36a93c61c8c49aa4f993f512fbea5b166db7591c
Andrew Hawn <blargyn@gmail.com>
2018-11-12 23:08:08 -0800

    Main Page UI Changes (Again)
    Made the same changes as before, but much faster and much sadder

--- Sahara/MainPage.xaml.cs
+++ Sahara/MainPage.xaml.cs
@@ -53,17 +53,28 @@ namespace Sahara
                 ItemTitle = "Skittles",
                 ItemDescription = "Wow it's candy.",
                 ItemPrice = 99.99,
+                ItemImage1 = "https://nuts.com/images/auto/801x534/assets/1c99a8a7052a8cb3.jpg",
             };
             var item2 = new ItemModel
             {
                 ItemId = 2,
                 ItemTitle = "Skittles But More",
-                ItemDescription = "Wow it's more candy. Like holy shit that's a lotta skittles.",
+                ItemDescription = "Wow it's more candy. Like holy crap that's a lotta skittles.",
                 ItemPrice = 999.99,
+                ItemImage1 = "http://images.tritondigitalcms.com/6616/sites/115/2018/01/19070509/skittles-300x168.jpg"
+            };
+            var item3 = new ItemModel
+            {
+                ItemId = 3,
+                ItemTitle = "Scrolling Test. Skittles are average at best.",
+                ItemDescription = "God these look disgusting.",
+                ItemPrice = 0.99,
+                ItemImage1 = "http://www.browniesfordays.com/wp-content/uploads/SKITTLES-BROWNIES-How-To-Make-Rainbow-Candy-Skittles-Brownies.jpg",
             };
 
             itemList.Add(item1);
             itemList.Add(item2);
+            itemList.Add(item3);
 
             HistoryListView.ItemsSource = itemList;
         }
