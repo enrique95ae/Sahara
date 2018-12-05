82b8b2ef514af519d55d044d33a2147f50312405
Aaron Brunette <apb120@gmail.com>
2018-12-05 10:24:55 -0800

    remade SearchResultPage
    cs file was disassociated with xaml file

--- Sahara/SearchResultPage.xaml.cs
+++ Sahara/SearchResultPage.xaml.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using Xamarin.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using ProtoBuf;
 
 namespace Sahara
 {
