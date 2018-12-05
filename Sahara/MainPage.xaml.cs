58ae36e74a1439cd6cf05ef5abf0d7aa548baf24
enrique95ae <enrique95ae@gmail.com>
2018-12-05 00:33:32 -0800

    NearbyItems

--- Sahara/MainPage.xaml.cs
+++ Sahara/MainPage.xaml.cs
@@ -21,6 +21,7 @@ namespace Sahara
         {
             InitializeComponent();
             PopulateItemListView();
+            PopulateItemNearby();
         }
 
         public static void CreateConnection()
@@ -119,6 +120,11 @@ namespace Sahara
                 ItemDescription = "There's festivals n stuff for this.",
                 ItemPrice = 5.99,
                 ItemImage1 = "https://hips.hearstapps.com/del.h-cdn.co/assets/18/09/2048x1024/landscape-1519653347-delish-roasted-asparagus-1.jpg?resize=1200:*",
+                ItemImage2 = "https://houseofnasheats.com/wp-content/uploads/2017/03/Oven-Roasted-Asparagus-with-Garlic-Parmesan-Lemon-8-1.jpg",
+                ItemImage3 = "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2009/6/12/1/IG0501_31034_s4x3.jpg.rend.hgtvcom.826.620.suffix/1384540892237.jpeg",
+                ItemImage4 = "https://www.thespruceeats.com/thmb/dfcgln-vBcf-OTGCjtgvQuxYjoA=/2000x1250/filters:no_upscale():max_bytes(150000):strip_icc()/Asparagus-584dcee63df78c491e8cd670.jpg",
+                ItemImage5 = "https://images.media-allrecipes.com/userphotos/560x315/1001600.jpg",
+                ItemImage6 = "https://www.twopeasandtheirpod.com/wp-content/uploads/2010/03/4477159251_51e896e29d.jpg",
             };
             var item2 = new ItemModel
             {
@@ -127,6 +133,12 @@ namespace Sahara
                 ItemDescription = "For grizzled veterans and manly men.",
                 ItemPrice = 3999.99,
                 ItemImage1 = "https://womencyclists.files.wordpress.com/2012/10/girl-bike.jpg",
+                ItemImage2 = "https://i5.walmartimages.com/asr/013696c9-6f7b-4bf9-85e4-1552551f77c9_1.5b9c045db8c9b56033b6000e71acc4ec.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
+                ItemImage3 = "https://app.skufetch.com/images.tmp/Royalbaby_Jenny_Princess_Pink_Girl_s_Bike_with_Training_Whee_4846_0_res.jpeg",
+                ItemImage4 = "http://www.ladypoolcycles.co.uk/image/cache/data/Moore%20Large/Cuda%2015/blox-14-pink-1200x1200.jpg",
+                ItemImage5 = "https://image.shutterstock.com/image-vector/kids-bicycle-girl-vector-illustration-260nw-95979451.jpg",
+                ItemImage6 = "https://i.ebayimg.com/images/g/~AcAAOSwq1JZJb1M/s-l300.jpg",
+
             };
             var item3 = new ItemModel
             {
@@ -135,14 +147,24 @@ namespace Sahara
                 ItemDescription = "Get swole, bruh.",
                 ItemPrice = 2.79,
                 ItemImage1 = "https://cdn.shopify.com/s/files/1/0944/0726/products/VPX-Bang-Energy-Drinks_2000x.jpg?v=1543424446",
+                ItemImage2 = "https://images-na.ssl-images-amazon.com/images/I/91gcW2%2BRVOL._SX355_.jpg",
+                ItemImage3 = "https://cdn.caffeineinformer.com/wp-content/caffeine/bang-energy-drink.jpg",
+                ItemImage4 = "https://cdn.shopify.com/s/files/1/1845/0375/products/bang_pina_colada_3_1024x1024.png?v=1518000398",
+                ItemImage5 = "https://cdn.shopify.com/s/files/1/2679/3646/products/rainbow_unicorn_grande_00f295fe-6d90-4be2-b938-2045afb5a184_2000x.jpg?v=1538701328",
+                ItemImage6 = "https://images-na.ssl-images-amazon.com/images/I/91av8P%2BNtIL._SX522_.jpg",
             };
             var item4 = new ItemModel
             {
                 ItemId = 4,
                 ItemTitle = "Buttons To Press",
-                ItemDescription = "We like buttons.",
+                ItemDescription = "You are bad at MMORPGs? you can get this and get it fixed. Andrew got it and got to Diamond in LoL.",
                 ItemPrice = 59.99,
                 ItemImage1 = "https://images-na.ssl-images-amazon.com/images/I/41ecZxB3MoL._SX425_.jpg",
+                ItemImage2 = "",
+                ItemImage3 = "",
+                ItemImage4 = "",
+                ItemImage5 = "",
+                ItemImage6 = "",
             };
 
             
@@ -214,7 +236,7 @@ namespace Sahara
             HistoryListView.ItemsSource = itemList;
         }
 
-        private void PopulateItemNearby()
+        public void PopulateItemNearby()
         {
             var nearbyItemList = new ObservableCollection<ItemModel>();
 
@@ -225,29 +247,27 @@ namespace Sahara
                 ItemDescription = "Specially designed for the hardest trails. Our team of engineers put a lot of effort into making the perfect mountain bike for all the " +
                     "contions",
                 ItemPrice = 3799.99,
-                ItemImage1 = "https://www.sweets-online.com/images/produkte/i12/12080-12080-1_1.jpg",
-                //ItemImage2 = "",
+                ItemImage1 = "https://images-na.ssl-images-amazon.com/images/I/51fxFwONaIL.jpg",
             };
 
             var nearbyItem2 = new ItemModel
             {
                 ItemId = 6,
-                ItemTitle = "Hardcore Mountain Bike",
-                ItemDescription = "Specially designed for the hardest trails. Our team of engineers put a lot of effort into making the perfect mountain bike for all the " +
-                    "contions",
-                ItemPrice = 3799.99,
-                ItemImage1 = "https://images-na.ssl-images-amazon.com/images/I/51fxFwONaIL.jpg",
+                ItemTitle = "Luxury Yatch",
+                ItemDescription = "For throwing legendary parties.",
+                ItemPrice = 999000.00,
+                ItemImage1 = "https://www.powerandmotoryacht.com/.image/c_limit%2Ccs_srgb%2Cfl_progressive%2Ch_700%2Cq_auto:good%2Cw_700/MTUwMTAyOTgzMTc5NTEwODQ2/hatteras100_prmjpg-promo-image.jpg",
                 //ItemImage2 = "",
             };
 
             var nearbyItem3 = new ItemModel
             {
                 ItemId = 7,
-                ItemTitle = "Hardcore Mountain Bike",
+                ItemTitle = "Reddit dope T-shirt",
                 ItemDescription = "Specially designed for the hardest trails. Our team of engineers put a lot of effort into making the perfect mountain bike for all the " +
                     "contions",
-                ItemPrice = 3799.99,
-                ItemImage1 = "https://images-na.ssl-images-amazon.com/images/I/51fxFwONaIL.jpg",
+                ItemPrice = 29.00,
+                ItemImage1 = "https://rlv.zcache.com/reddit_alien_t_shirt-rc1ed25c8ba2f4b36a9179090d42439c1_k2gl9_307.jpg",
                 //ItemImage2 = "",
             };
 
@@ -258,6 +278,8 @@ namespace Sahara
             nearbyItemList.Add(nearbyItem3);
 
             nearbyItem1Image.Source = nearbyItem1.ItemImage1;
+            nearbyItem2Image.Source = nearbyItem2.ItemImage1;
+            nearbyItem3Image.Source = nearbyItem3.ItemImage1;
 
 
         }
@@ -267,6 +289,29 @@ namespace Sahara
             var menuItem = (Button)sender;
             var item = (ItemModel)menuItem.CommandParameter;
             Navigation.PushAsync(new ItemPage(item));
+        }
+
+        void Handle_NearbyItem1Click(object sender, System.EventArgs e) //Navigates to a page with more detail
+        {
+            var nearbyItem1 = new ItemModel
+            {
+                ItemId = 5,
+                ItemTitle = "Hardcore Mountain Bike",
+                ItemDescription = "Specially designed for the hardest trails. Our team of engineers put a lot of effort into making the perfect mountain bike for all the " +
+                 "contions",
+                ItemPrice = 3799.99,
+                ItemImage1 = "https://images-na.ssl-images-amazon.com/images/I/51fxFwONaIL.jpg",
+                ItemImage2 = "https://i5.walmartimages.com/asr/013696c9-6f7b-4bf9-85e4-1552551f77c9_1.5b9c045db8c9b56033b6000e71acc4ec.jpeg?odnHeight=450&odnWidth=450&odnBg=FFFFFF",
+                ItemImage3 = "https://app.skufetch.com/images.tmp/Royalbaby_Jenny_Princess_Pink_Girl_s_Bike_with_Training_Whee_4846_0_res.jpeg",
+                ItemImage4 = "http://www.ladypoolcycles.co.uk/image/cache/data/Moore%20Large/Cuda%2015/blox-14-pink-1200x1200.jpg",
+                ItemImage5 = "https://image.shutterstock.com/image-vector/kids-bicycle-girl-vector-illustration-260nw-95979451.jpg",
+                ItemImage6 = "https://i.ebayimg.com/images/g/~AcAAOSwq1JZJb1M/s-l300.jpg",
+            };
+
+
+            var menuItem = (Button)sender;
+            var item = (ItemModel)menuItem.CommandParameter;
+            Navigation.PushAsync(new ItemPage(nearbyItem1));
         }
 
     }
